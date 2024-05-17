d8916a11b1bf5b5d5b9d91d5f48eb5d196a757a3
sophiealyson99 <st3385@drexel.edu>
2024-05-17 14:40:37 -0400

    following nav demo to 23 min

---
+++ js/scripts-dist.js
@@ -0,0 +1,1 @@
+var burgerBtn=document.querySelector("#burger-container button");burgerBtn.addEventListener("click",(()=>{console.log("burger clicked")}));
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var burgerBtn = document.querySelector("#burger-container button");
+
+burgerBtn.addEventListener("click", () =>{
+    console.log("burger clicked");
+});
